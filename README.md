4c6342f55b00c5e3182a147aaa5f3d436cf0315c
TyPe6000 <162706457+TyPe6000@users.noreply.github.com>
2025-02-17 17:55:43 +0900

    log250217

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rust
 Rust 프로그래밍 언어 개인학습 기록 / Record of Studying Rust Programming Language
+
+---
+
+### 참고자료
+
+https://doc.rust-kr.org/
---
+++ projects/ch01_001_hello_cargo/src/main.rs
@@ -0,0 +1,13 @@
+// 1.3. 카고를 사용해봅시다
+fn main() {
+    println!("Hello, world!");
+}
+
+// $ cargo new ch001_hello_cargo
+// 새로운 프로젝트 생성
+// $ cargo build 
+// 프로젝트 빌드(컴파일), target/debug 디렉토리에 실행 파일 생성
+// $ cargo run
+// 프로젝트 빌드 후 실행
+// $ cargo check
+// 프로젝트 빌드 없이 컴파일 체크만 수행
